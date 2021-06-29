0acd4d786b8f0531b38f4289922857df23267b3d
DinossauroBebado <guipsilva32@gmail.com>
2021-06-29 20:35:25 -0300

    desvio padrão e mais umas coisas

--- media_aritimetica.py
+++ media_aritimetica.py
@@ -4,6 +4,26 @@ somah = 0
 soma = 0
 cont = 0
 mult = 1
+numeros = []
+
+
+def variancia(numeros, media):
+    sum = 0
+    for numero in numeros:
+        sum = sum + (numero-media)**2
+    var = (1/(len(numeros)-1)*sum)
+    return var
+
+
+def desvio_padrao(numeros, media):
+    return variancia(numeros, media)**(1/2)
+
+
+def percentil(numeros, pos):
+    q = (pos/100)*(len(numeros)+1)
+    alfa = int(str(q)[0:str(q).index(".")])
+    beta = float(str(q)[str(q).index(".")+1:])/10
+    return q
 
 
 def media_harmonica(somaInversa, cont):
@@ -21,7 +41,9 @@ with open('sum.txt', 'r') as inp:
 
 for number in numbers:
     soma = soma + float(number.replace(",", "."))
-    somah = somah + 1/float(number.replace(",", "."))
+    if(float(number.replace(",", ".")) != 0):
+        somah = somah + 1/float(number.replace(",", "."))
+    numeros.append(float(number.replace(",", ".")))
     mult = mult*((float(number.replace(",", ".")))/100 + 1)
     cont += 1
 
@@ -31,3 +53,8 @@ print("n: " + str(cont))
 print("media arimetica: " + str(soma/cont))
 print("media Harmonica " + str(media_harmonica(somah, cont)))
 print("media geometrica: " + str(media_geometrica(mult, cont)))
+
+# para calcular moderadas porcentils e tals
+print(sorted(numeros))
+
+print("desviopadrao: " + str(desvio_padrao(numeros, soma/cont)))
